567b8f4ae4d1568dc83c9d343d0bb5c7a39c9a2f
qedized <qedized@gmail.com>
2021-11-20 08:31:50 -0500

    Added Checkerboard
    Yes.

--- main.cpp
+++ main.cpp
@@ -69,7 +69,21 @@ bool scene_intersect(const vec3 &orig, const vec3 &dir, const std::vector<Sphere
             material = spheres[i].material;				// mutate material to show it later
         }
     }
-    return spheres_dist < 1000;
+
+    float checkerboard_dist = std::numeric_limits<float>::max();
+    if (fabs(dir.y) > 1e-3)  {
+        float d = -(orig.y+4) / dir.y; // the checkerboard plane has equation y = -4
+        vec3 pt = orig + dir * d;
+        if (d > 0 && fabs(pt.x) < 10 && pt.z > 10 && pt.z < 30 && d < spheres_dist) {
+            checkerboard_dist = d;
+            hit = pt;
+            N = vec3{0, 1, 0};
+            material.diffuse_color = (int(.5 * hit.x + 1000) + int(.5 * hit.z)) & 1 ? vec3{1, 1, 1} : vec3{1, .7, .3};
+            material.diffuse_color = material.diffuse_color * .3;
+        }
+    }
+
+    return std::min(spheres_dist, checkerboard_dist) < 1000;
 }
 
 vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, size_t depth = 0) {
