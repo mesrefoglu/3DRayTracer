3d7d2d235d829fe9e615d5b74fa6af25e11c9dc6
qedized <qedized@gmail.com>
2021-11-20 03:31:04 -0500

    Creating a picture in ppm format
    Only creating the picture

--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@
 #include "geometry.h"
 
 void render() {
-    const int width    = 1024;
-    const int height   = 768;
-    std::vector<Vec3f> framebuffer(width*height);
+    const int height = 768;
+    const int width = 1024;
+    std::vector<vec3> framebuffer(width * height);
 
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
-            framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width), 0);
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j <width; j++) {
+            // each x value of framebuffer is a float between 0 and 1 referring to the height, and y is width
+            framebuffer[i * width + j] = vec3{i / float(height), j / float(width), 0};
         }
     }
 
     std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        for (size_t j = 0; j<3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
+    ofs.open("./out.ppm", std::ofstream::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n"; // set he ppm file properties
+    for (vec3& c : framebuffer) {
+        ofs << (char)(int)(255.f * c[0]) // red
+            << (char)0 // green
+            << (char)(int)(255.f * c[1]); // blue
     }
     ofs.close();
 }
